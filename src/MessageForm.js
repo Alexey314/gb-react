ef44ac3fb17f8850c95f98e808a435b9cd80ed30
Aleksey Potapov <apotapovxx@mail.ru>
2021-07-22 21:39:58 +0300

    refactor: use onSubmit in form of MessageForm

--- src/MessageForm.js
+++ src/MessageForm.js
@@ -1,10 +1,9 @@
 import "./MessageForm.css";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
 import { makeStyles } from "@material-ui/core/styles";
 import Icon from "@material-ui/core/Icon";
-import send from "@material-ui/icons/Send";
 
 const useStyles = makeStyles((theme) => ({
   button: {
@@ -14,35 +13,38 @@ const useStyles = makeStyles((theme) => ({
 
 function MessageForm(props) {
   const classes = useStyles();
-  const inputEl = useRef(null);
   const [message, setMessage] = useState("");
   const handleMessageChange = (event) => {
     setMessage(event.target.value);
   };
   const onSend = (e) => {
     e.preventDefault();
-    const text = String(message);
-    if (text.length) {
-      props.onSend(text);
+    if (message.length) {
+      props.onSend(message);
     }
   };
 
   return (
-    <form className="MessageForm" noValidate autoComplete="off">
+    <form
+      className="MessageForm"
+      onSubmit={onSend}
+      noValidate
+      autoComplete="off"
+    >
       <TextField
         className="MessageForm-msg"
-        type="password"
+        type="text"
         value={message}
         onChange={handleMessageChange}
         variant="outlined"
         placeholder="Input message here"
       />
       <Button
+        type="submit"
         variant="contained"
         color="primary"
         className={classes.button}
         endIcon={<Icon>send</Icon>}
-        onClick={onSend}
       >
         Send
       </Button>
